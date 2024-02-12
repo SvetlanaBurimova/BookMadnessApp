7b4fc0046990b49526b4bbda89d683882f55ef00
Svetlana Burimova <39984764+SvetlanaBurimova@users.noreply.github.com>
2024-02-12 14:33:57 +0200

    Add home view model (#3)
    Added home view model and passed it into home screen

---
+++ app/src/main/java/com/example/book_madness/ui/home/HomeViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.book_madness.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.book_madness.data.Book
+import com.example.book_madness.data.BooksRepository
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+data class HomeUiState(val bookList: List<Book> = listOf())
+
+class HomeViewModel(booksRepository: BooksRepository) : ViewModel() {
+    val homeUiState: StateFlow<HomeUiState> =
+        booksRepository.getAllBooksStream().map { HomeUiState(it) }
+            .stateIn(
+                scope = viewModelScope,
+                started = WhileSubscribed(),
+                initialValue = HomeUiState()
+            )
+}
