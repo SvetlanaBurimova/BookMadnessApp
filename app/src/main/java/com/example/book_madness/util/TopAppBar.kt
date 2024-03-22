998450fe50f8ce85ffeec0db721eb1d069e522e7
svetlanaburimova <svetaburimova@gmail.com>
2024-03-22 17:39:12 +0200

    Added search logic and moved it to top app bar

--- app/src/main/java/com/example/book_madness/util/TopAppBar.kt
+++ app/src/main/java/com/example/book_madness/util/TopAppBar.kt
@@ -3,12 +3,16 @@ package com.example.book_madness.util
 import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
@@ -17,54 +21,70 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import com.example.book_madness.R
 import com.example.book_madness.ui.bookItem.BookDetailsUiState
+import com.example.book_madness.ui.theme.AppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookMadnessTopAppBar(
     title: String,
     modifier: Modifier = Modifier,
+    searchQuery: String = "",
     canNavigateBack: Boolean = true,
     showFilterIcon: Boolean = false,
     showShareButton: Boolean = false,
-    bookDetailsUiState: BookDetailsUiState = BookDetailsUiState(),
+    showSearchButton: Boolean = false,
+    expandedInitially: Boolean = false,
     onFilterAllBooks: () -> Unit = {},
     onFilterAllBooksByName: () -> Unit = {},
     onFilterAllBooksByRating: () -> Unit = {},
     onFilterAllBooksByTBR: () -> Unit = {},
     onFilterAllBooksByYear2023: () -> Unit = {},
     onFilterAllBooksByYear2024: () -> Unit = {},
-    navigateUp: () -> Unit = {}
+    onSearchDisplayChanged: (String) -> Unit = {},
+    navigateUp: () -> Unit = {},
+    bookDetailsUiState: BookDetailsUiState = BookDetailsUiState(),
 ) {
     CenterAlignedTopAppBar(
         title = { Text(title) },
         navigationIcon = {
             if (canNavigateBack) {
                 IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
+                    BackArrowIcon()
                 }
             }
         },
         actions = {
+            if (showSearchButton)
+                ExpandableSearchView(
+                    searchQuery = searchQuery,
+                    onSearchDisplayChanged = onSearchDisplayChanged,
+                    expandedInitially = expandedInitially,
+                    modifier = modifier.weight(1f)
+                )
             if (showFilterIcon)
-                FilterBooksMenu(
+                DropdownBookFilters(
                     onFilterAllBooks,
                     onFilterAllBooksByName,
                     onFilterAllBooksByRating,
@@ -72,12 +92,11 @@ fun BookMadnessTopAppBar(
                     onFilterAllBooksByYear2023,
                     onFilterAllBooksByYear2024
                 )
-            if (showShareButton) {
+            if (showShareButton)
                 ShareButton(
                     bookDetailsUiState,
                     LocalContext.current
                 )
-            }
         },
         modifier = modifier
             .background(MaterialTheme.colorScheme.outline)
@@ -128,13 +147,84 @@ fun ShareButton(
 }
 
 @Composable
-private fun FilterBooksMenu(
+fun ExpandableSearchView(
+    searchQuery: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    expandedInitially: Boolean = false,
+) {
+    val (expanded, onExpandedChanged) = remember {
+        mutableStateOf(expandedInitially)
+    }
+
+    when (expanded) {
+        true -> ExpandedSearchView(
+            onSearchDisplayChanged = onSearchDisplayChanged,
+            onExpandedChanged = onExpandedChanged,
+            searchQuery = searchQuery,
+            modifier = modifier
+        )
+        false -> {
+            onSearchDisplayChanged("")
+            IconButton(
+                onClick = { onExpandedChanged(true) }
+            ) {
+                SearchIcon()
+            }
+        }
+    }
+}
+
+@Composable
+fun ExpandedSearchView(
+    searchQuery: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    onExpandedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val textFieldFocusRequester = remember { FocusRequester() }
+
+    SideEffect { textFieldFocusRequester.requestFocus() }
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        IconButton(
+            onClick = { onExpandedChanged(false) }
+        ) { BackArrowIcon() }
+        TextField(
+            value = searchQuery,
+            onValueChange = { onSearchDisplayChanged(it) },
+            modifier = Modifier.focusRequester(textFieldFocusRequester),
+            label = { Text(text = stringResource(id = R.string.search)) },
+            trailingIcon = {
+                if (searchQuery.isNotEmpty()) {
+                    IconButton(
+                        onClick = { onSearchDisplayChanged("") }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            tint = MaterialTheme.colorScheme.onSurface,
+                            contentDescription = null
+                        )
+                    }
+                }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
+        )
+    }
+}
+
+@Composable
+private fun DropdownBookFilters(
     onFilterAllBooks: () -> Unit,
     onFilterAllBooksByName: () -> Unit,
     onFilterAllBooksByRating: () -> Unit,
     onFilterAllBooksByTBR: () -> Unit,
     onFilterAllBooksByYear2023: () -> Unit,
-    onFilterAllBooksByYear2024: () -> Unit,
+    onFilterAllBooksByYear2024: () -> Unit
 ) {
     TopAppBarDropdownMenu(
         iconContent = {
@@ -178,16 +268,45 @@ private fun TopAppBarDropdownMenu(
 ) {
     var expanded by remember { mutableStateOf(false) }
 
-    Box(modifier = Modifier.wrapContentSize(Alignment.TopEnd)) {
+    Box {
         IconButton(onClick = { expanded = !expanded }) {
             iconContent()
         }
         DropdownMenu(
             expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier.wrapContentSize(Alignment.TopEnd)
+            onDismissRequest = { expanded = false }
         ) {
             content { expanded = !expanded }
+        }
+    }
+}
+
+@Composable
+fun BackArrowIcon() {
+    Icon(
+        imageVector = Icons.Default.ArrowBack,
+        contentDescription = null
+    )
+}
+
+@Composable
+fun SearchIcon() {
+    Icon(
+        imageVector = Icons.Default.Search,
+        contentDescription = null
+    )
+}
+
+@Preview
+@Composable
+fun ExpandedSearchViewPreview() {
+    AppTheme {
+        Surface {
+            ExpandableSearchView(
+                onSearchDisplayChanged = {},
+                expandedInitially = true,
+                searchQuery = ""
+            )
         }
     }
 }
