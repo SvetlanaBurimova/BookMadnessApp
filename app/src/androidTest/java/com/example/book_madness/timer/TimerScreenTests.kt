b9724152a36c4d783aa2a47d50cd80c511debe8f
Svetlana Burimova <39984764+SvetlanaBurimova@users.noreply.github.com>
2024-03-30 17:17:37 +0200

    Created timer screen UI tests (#40)

---
+++ app/src/androidTest/java/com/example/book_madness/timer/TimerScreenTests.kt
@@ -0,0 +1,63 @@
+package com.example.book_madness.timer
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Surface
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.book_madness.R
+import com.example.book_madness.data.WorkManagerRepository
+import com.example.book_madness.onNodeWithStringId
+import com.example.book_madness.ui.theme.AppTheme
+import com.example.book_madness.ui.timer.CountDownTimerScreen
+import com.example.book_madness.ui.timer.CountTimerViewModel
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TimerScreenTests {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Before
+    fun setContent() {
+        composeTestRule.setContent {
+            AppTheme {
+                Surface {
+                    CountDownTimerScreen(
+                        viewModel = CountTimerViewModel(
+                            WorkManagerRepository(ApplicationProvider.getApplicationContext())
+                        ),
+                        bottomNavigationBar = { },
+                    )
+                }
+            }
+        }
+    }
+
+    @Test
+    fun displayTimerScreenElements() = runTest {
+        composeTestRule.onNodeWithStringId(R.string.reading_time_title).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.enjoy_title).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.timer_description).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.start_button).assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.reminder_button).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription("Timer progress indicator").assertIsDisplayed()
+        composeTestRule.onNodeWithText("30:00").assertIsDisplayed()
+    }
+
+    @Test
+    fun startTimer_timeIsChanged() = runTest {
+        composeTestRule.onNodeWithText("30:00").assertIsDisplayed()
+        composeTestRule.onNodeWithStringId(R.string.start_button).performClick()
+        composeTestRule.onNodeWithText("30:00").assertDoesNotExist()
+    }
+}
