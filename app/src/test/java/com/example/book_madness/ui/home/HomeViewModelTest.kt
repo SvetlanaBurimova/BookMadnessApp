b87a8e04ca734c6f46684ec1deb5d0b4442f0394
svetlanaburimova <svetaburimova@gmail.com>
2024-04-05 22:55:54 +0300

    Added unit test for home view model

---
+++ app/src/test/java/com/example/book_madness/ui/home/HomeViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.example.book_madness.ui.home
+
+import com.example.book_madness.data.BooksRepository
+import com.example.book_madness.model.FilterType
+import com.example.book_madness.util.fakeData.FakeDataSource.bookOne
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+class HomeViewModelTest {
+
+    @Mock
+    private lateinit var booksRepository: BooksRepository
+
+    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        viewModel = HomeViewModel(booksRepository)
+    }
+
+    @Test
+    fun filterBooks_updatesFilterType() {
+        viewModel.filterBooks(FilterType.NAME)
+        assertEquals(FilterType.NAME, viewModel.filterType.value)
+    }
+
+    @Test
+    fun onSearchQueryChange_updatesSearchQuery() {
+        viewModel.onSearchQueryChange("New Query")
+        assertEquals("New Query", viewModel.searchQuery)
+    }
+
+    @Test
+    fun deleteBook_callsRepository() = runBlocking {
+        viewModel.deleteBook(bookOne)
+
+        verify(booksRepository).deleteBook(bookOne)
+    }
+}
